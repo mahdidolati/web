9e179ac9e06e9f88f9cb29bdff0ce3e8a0d7952d
Mahdi <mehdidolati@hotmail.com>
2021-04-20 23:53:19 +0430

    Show field error messages

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -8,17 +8,25 @@ import { Box } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 import { useMutation } from "urql";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps { }
 
 export const Register: React.FC<registerProps> = ({}) => {
+    const router = useRouter();
     const [,register] = useRegisterMutation();
     return (
         <Wrapper variant="small">
             <Formik 
                 initialValues={{ username: "", password: ""}}
-                onSubmit={async (values) => {
+                onSubmit={async (values, {setErrors}) => {
                     const response = await register(values);
+                    if(response.data?.register.errors) {
+                        setErrors(toErrorMap(response.data.register.errors));
+                    } else if (response.data?.register.user) {
+                        router.push("/");
+                    }
                 }}
             >
                 {({ isSubmitting }) => (
